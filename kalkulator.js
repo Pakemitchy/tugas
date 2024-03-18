c5ca95b7bed38739c1f82511e56357e17ed5832c
Pakemitchy <aaway079@gmail.com>
2024-03-18 21:29:05 +0700

    v1.0

---
+++ kalkulator.js
@@ -0,0 +1,16 @@
+function hitung() {
+    const angka1 = document.getElementById("angka1").value;
+    const operator = document.getElementById("operator").value;
+    const angka2 = document.getElementById("angka2").value;
+  
+    let hasil;
+  
+    switch (operator) {
+      case "+":
+        hasil = parseInt(angka1) + parseInt(angka2);
+        break;
+    }
+  
+    document.getElementById("hasil").innerHTML = hasil;
+  }
+  
